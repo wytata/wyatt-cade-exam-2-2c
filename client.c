90e13df3aceb8a5530a7a2463bb5bf2f802f9f21
Wyatt Cade <wyattrcade@gmail.com>
2023-04-07 00:41:31 +0000

    output looks correct

--- client.c
+++ client.c
@@ -17,6 +17,7 @@ int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
     struct sockaddr_in server_address;
+    struct sockaddr_in hints;
     struct hostent *server;
     char buffer[256];
 
@@ -30,16 +31,22 @@ int main(int argc, char *argv[])
      Read the server port number
     */
     // TODO
+    portno = atoi(argv[2]);
 
     /*
      Create a socket
     */ 
     // TODO
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        error("Could not open socket.");
+    }
 
     /*
     Read the server hostname
     */   
     // TODO
+    server = gethostbyname(argv[1]);
 
     /*
     The function bzero() sets all values in a buffer to zero.
@@ -51,12 +58,20 @@ int main(int argc, char *argv[])
     Configure the server_address struct accordingly.
     */
     // TODO
+    bcopy((char *)server->h_addr, (char *)&server_address.sin_addr.s_addr, server->h_length);
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(portno);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    
 
     /*
     Establish a connection to the server using the socket and the server_address configured above.
     */
     // TODO
-    
+    int connectResult = connect(sockfd, (struct sockaddr *)&server_address, sizeof(server_address));
+    if (connectResult < 0) {
+        error("connection unsuccessful");
+    }
     /*
     Prompt the user to enter a message, use fgets to read the message from stdin.
     */
@@ -68,11 +83,20 @@ int main(int argc, char *argv[])
     Write the message above to the socket.
     */
     // TODO
+    int writeResult = write(sockfd, buffer, sizeof(buffer));
+    if (writeResult < 0) {
+        error("Write to socket failed.");
+    }
 
     /*
     Read the reply from the socket.
     */
     // TODO
+    bzero(buffer, 256);
+    n = read(sockfd, buffer, 256);
+    if (n < 0) {
+        error("Read from socket failed.");
+    }
 
     /*
      Print the server response and close the socket
